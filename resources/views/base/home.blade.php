f154e1219b153fa6635db84a57f02098c1b614e7
vladino <vladino.vrab@gmail.com>
2018-08-27 20:20:15 +0200

    Resume layout prepared

--- resources/views/base/home.blade.php
+++ resources/views/base/home.blade.php
@@ -28,22 +28,38 @@
     <section class="base-section base-section-dark">
         <section class="inner">
         <h1>Drugs a developer like me is addicted to</h1>
-            <p>
-                There are many variations of passages of Lorem Ipsum available,
-                but the majority have suffered alteration in some form, by injected
-                humour, or randomised words which
-                <br><br>
-                It is a long established fact that a reader will be distracted by the
-                readable content of a page when looking at its layout. The point of
-                using Lorem Ipsum is that it has a more-or-less normal distribution of
-                letters, as opposed to using 'Content here, content here', making it
-                look like readable English. Many desktop publishing packages and web
-                page editors now use Lorem Ipsum as their default model text,
-                <br><br>And a for 'lorem ipsum' will uncover many web sites still in their
-                infancy. Various versions have evolved over the years, sometimes by
-                accident, sometimes on purpose (injected humour and the like).
-            </p>
-            <a class="hvr-bounce-to-right" href="">RESUME</a>
+        <div class="drugs-box row">
+            <div class="drugs-box-inner col-md-2">
+                <span class="fas fa-kiss-wink-heart"></span>
+                <h3>Love</h3>
+                <p>No man can live without a love</p>
+            </div>
+            <div class="drugs-box-inner col-md-2">
+                <span class="fas fa-home"></span>
+                <h3>Home</h3>
+                <p>Place where we come no matter how hard we failed</p>
+            </div>
+            <div class="drugs-box-inner col-md-2">
+                <span class="fas fa-user-friends"></span>
+                <h3>Friends</h3>
+                <p>People we choose to be with</p>
+            </div>
+            <div class="drugs-box-inner col-md-2">
+                <span class="fas fa-laptop-code"></span>
+                <h3>Coding</h3>
+                <p>Love what you do. Love what you developer. Love the value.</p>
+            </div>
+            <div class="drugs-box-inner col-md-2">
+                <span class="fas fa-paw"></span>
+                <h3>Dog</h3>
+                <p>Somebody who teaches us to be loyal, good and always happy.</p>
+            </div>
+            <div class="drugs-box-inner col-md-2">
+                <span class="fas fa-car"></span>
+                <h3>Car</h3>
+                <p>Speed, technologies and the beauty on 4 wheels.</p>
+            </div>
+        </div>
         </section>
     </section>
 
--- resources/views/base/resume.blade.php
+++ resources/views/base/resume.blade.php
@@ -2,7 +2,103 @@
 
 @section("content")
 
+    <section class="base-section text-left">
+        <section class="inner">
+            <div class="row">
+                <div class="col-md-5">
+                    <h3>Vladimír Vráb</h3>
+                    <p>
+                        Martin (Slovakia), 19th of May 1993<br>
+                        Nationality: Slovak<br>
+                        Email: vladino.vrab@gmail.com<br>
+                        Phone: (Slovak) 00421 902 739 429<br>
+                        Driving license B<br>
+                    </p>
+                </div>
+                <div class="my-face" style="background-image: url({{assetn("images/my-face.jpeg")}})"></div>
+            </div>
+        </section>
+    </section>
 
+    <section class="base-section text-left alter-margin">
+        <section class="inner">
+            <div class="special-line">
+                <div class="line"></div>
+                <h2>SUMMARY</h2>
+                <p></p>
+            </div>
+        </section>
+    </section>
+
+    <section class="base-section text-left alter-margin">
+        <section class="inner">
+            <div class="special-line">
+                <div class="line"></div>
+                <h2>EDUCATION</h2>
+                <p></p>
+            </div>
+        </section>
+    </section>
+
+    <section class="base-section text-left alter-margin">
+        <section class="inner">
+            <div class="special-line">
+                <div class="line"></div>
+                <h2>WORK HISTORY</h2>
+                <p></p>
+            </div>
+        </section>
+    </section>
+
+    <section class="base-section text-left alter-margin">
+        <section class="inner">
+            <div class="special-line">
+                <div class="line"></div>
+                <h2>TECHNICAL SKILLS</h2>
+                <p></p>
+            </div>
+        </section>
+    </section>
+
+    <section class="base-section text-left alter-margin">
+        <section class="inner">
+            <div class="special-line">
+                <div class="line"></div>
+                <h2>MORE ABOUT TECHNICAL SKILLS</h2>
+                <p></p>
+            </div>
+        </section>
+    </section>
+
+    <section class="base-section text-left alter-margin">
+        <section class="inner">
+            <div class="special-line">
+                <div class="line"></div>
+                <h2>PROJECTS I TOUCHED</h2>
+                <p></p>
+            </div>
+        </section>
+    </section>
+
+    <section class="base-section text-left alter-margin">
+        <section class="inner">
+            <div class="special-line">
+                <div class="line"></div>
+                <h2>LANGUAGES</h2>
+                <p></p>
+            </div>
+        </section>
+    </section>
+
+    <section class="base-section text-left alter-margin">
+        <section class="inner">
+            <div class="special-line">
+                <div class="line"></div>
+                <h2>INTEREST & HOBBIES</h2>
+                <p></p>
+            </div>
+        </section>
+    </section>
 
 
 @endsection
