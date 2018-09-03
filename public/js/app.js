cbd5e2a0af32a929b8718cc04dbb94246a55561a
vladino <vladino.vrab@gmail.com>
2018-09-03 22:14:55 +0200

    ios fix

--- public/js/app.js
+++ public/js/app.js
@@ -25,19 +25,20 @@ $(function() {
         }, 1000);
     }, 4000);
 
-    $(".navigation-mobile").on("click", function() {
+    // iOS fix
+    $(".navigation-mobile").css("cursor", "pointer");
+    $(".close-mobile-navigation span").css("cursor", "pointer");
 
+    $(document).on("click", ".navigation-mobile", function() {
         $("body").addClass("mobile-overflow");
         $(".navigation-mobile-content").fadeIn(250);
-
     });
 
-    $(".close-mobile-navigation span").on("click", function() {
-
+    $(document).on("click", ".close-mobile-navigation span", function() {
         $(".navigation-mobile-content").fadeOut(250, function() {
             $("body").removeClass("mobile-overflow");
         });
-
     });
+
 
 });
